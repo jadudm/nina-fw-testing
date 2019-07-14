88452b947d7fb7b8c3fe47a3cfbf9cafcef1586c
Matthew Jadud <matt@jadud.com>
2019-07-14 08:31:26 +0200

    Uses new abstraction.
    This allows the user to construct an I2C or SPI object, and pass that to the ESP32 control.

--- test_with_improved_i2c_lib.py
+++ test_with_improved_i2c_lib.py
@@ -2,10 +2,9 @@ import board
 import busio
 from digitalio import DigitalInOut
 from digitalio import Direction
-
 from time import sleep
 import neopixel, random
-from adafruit_esp32i2c import adafruit_esp32i2c
+from adafruit_esp32i2c import adafruit_esp32
 
 pxl = neopixel.NeoPixel(board.NEOPIXEL, 1)
 
@@ -13,9 +12,14 @@ pxl = neopixel.NeoPixel(board.NEOPIXEL, 1)
 # MOSI is ...
 # MISO is ...
 # spi = busio.SPI(board.SCK, board.MOSI, board.MISO)
-esp = adafruit_esp32i2c.ESP_I2Ccontrol(None, None, None, None, debug = False)
+
+params = {'SCL' : board.SCL, 'SDA' : board.SDA, 'address' : 0x2A}
+
+protocol = adafruit_esp32.I2C(params, None, None, None, debug=3)
+esp = adafruit_esp32.ESP_Control(protocol)
+
 esp.set_pin_mode(13, Direction.OUTPUT)
-SLEEP_TIME = 0.1
+SLEEP_TIME = 0.5
 while True:
   esp.set_digital_write(13, 1)
   sleep(SLEEP_TIME)
