2de5b9ff23d1d64e37358771d97a6b7396213625
Matthew Jadud <matt@jadud.com>
2019-07-16 09:52:29 +0200

    Working through close socket...
    But, why does that fail?

--- jadud-i2c-test.py
+++ jadud-i2c-test.py
@@ -16,7 +16,7 @@ params = {'SCL' : board.SCL, 'SDA' : board.SDA, 'address' : 0x2A}
 protocol = adafruit_esp32.I2C(params, 
                               ready_pin=DigitalInOut(board.D5), 
                               reset_pin=DigitalInOut(board.D6),
-                              debug=False)
+                              debug=True)
 
 esp = adafruit_esp32.ESP_Control(protocol)
 requests.set_interface(esp)
@@ -25,24 +25,28 @@ print("ESP32 I2C webclient test")
 
 if esp.status == adafruit_esp32.WL_IDLE_STATUS:
     print("ESP32 found and in idle mode")
+
 print("Firmware vers.", esp.firmware_version)
 print("MAC addr:", [hex(i) for i in esp.MAC_address])
 
-esp.set_digital_write(13, 1)
-time.sleep(1)
-esp.set_digital_write(13, 0)
-time.sleep(1)
+#for ap in esp.scan_networks():
+#    print("\t%s\t\tRSSI: %d" % (str(ap['ssid'], 'utf-8'), ap['rssi']))
 
-for ap in esp.scan_networks():
-    print("\t%s\t\tRSSI: %d" % (str(ap['ssid'], 'utf-8'), ap['rssi']))
 
 print("Connecting to AP...")
+time.sleep(1)
+# esp.connect_AP("phnphn", "nutnutnut")
+esp.connect_AP(config.ssid, config.password)
+time.sleep(5)
+
 while not esp.is_connected:
     try:
         esp.connect_AP(config.ssid, config.password)
+        time.sleep(5)
     except RuntimeError as e:
         print("could not connect to AP, retrying: ",e)
         continue
+
 print("Connected to", str(esp.ssid, 'utf-8'), "\tRSSI:", esp.rssi)
 print("My IP address is", esp.pretty_ip(esp.ip_address))
 print("IP lookup adafruit.com: %s" % esp.pretty_ip(esp.get_host_by_name("adafruit.com")))
